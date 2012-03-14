4a20c9f6d1b47d8f2ee5bc32fcfe917fc089ba83
David L. Goldberg <davidlgoldberg@gmail.com>
2012-03-14 11:24:37 -0400

    adds templating with jinja.

--- app.py
+++ app.py
@@ -25,11 +25,11 @@ def page_not_found(e):
 
 @app.route('/')
 def index():
-    return render_template('index')
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
-    return render_template('about')
+    return render_template('about.html')
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
